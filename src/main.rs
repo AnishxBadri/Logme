0c5cfb82d9de747d712834b8be2a6802f9bacfae
AnishxBadri <anishkb2001@gmail.com>
2024-10-12 23:45:07 +0400

    feat: basic ratatui setup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
-use logme::run;
+use std::io;
 
-fn main() {
-    run();
+use ratatui::{
+    crossterm::event::{self, KeyCode, KeyEventKind},
+    style::Stylize,
+    widgets::Paragraph,
+    DefaultTerminal,
+};
+
+fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
+    loop {
+        terminal.draw(|frame| {
+            let greeting = Paragraph::new("Hello Logme! (press 'q' to quit)")
+                .white()
+                .on_black();
+            frame.render_widget(greeting, frame.area());
+        })?;
+
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(());
+            }
+        }
+    }
+}
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    terminal.clear()?;
+    let app_result = run(terminal);
+    ratatui::restore();
+    app_result
 }
